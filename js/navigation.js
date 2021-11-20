96451db76f29d2b6ebb6453ce56c83db24a8f196
femmyte <sanyaoluadefemi@gmail.com>
2021-11-20 11:16:12 +0800

    changed the active state of the hamburger

--- js/navigation.js
+++ js/navigation.js
@@ -1,9 +1,10 @@
-const hamburger = document.getElementById("hamburger");
-const navGroup = document.getElementById("nav-group");
-const navTriangle = document.getElementById("nav-triangle");
+const hamburger = document.getElementById('hamburger')
+const navGroup = document.getElementById('nav-group')
+const navTriangle = document.getElementById('nav-triangle')
 
-hamburger.addEventListener("click", () => {
-  console.log("clicked");
-  navGroup.classList.toggle("active");
-  navTriangle.classList.toggle("active");
-});
+hamburger.addEventListener('click', () => {
+  console.log('clicked')
+  hamburger.classList.toggle('myactive')
+  navGroup.classList.toggle('active')
+  navTriangle.classList.toggle('active')
+})
